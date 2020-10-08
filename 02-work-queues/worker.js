b72df281835b8e9925cd225cca10d309bdea64ce
Bruno Bigotto <bbigotto@gmail.com>
2020-10-08 16:59:47 -0400

    Work queues

---
+++ 02-work-queues/worker.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = 'task_queue';
+
+    channel.assertQueue(queue, {
+      durable: true
+    });
+    channel.prefetch(1);
+    channel.consume(queue, function(msg) {
+        const secs = msg.content.toString().split('.').length - 1;
+
+        console.log(`[x] Received ${msg.content.toString()}`);
+        setTimeout(function() {
+            console.log(' [x] Done');
+            channel.ack(msg);
+        }, secs * 1000);
+    }, {
+        noAck: false
+    });
+  });
+});
